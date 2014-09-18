9948882f57be08bf635203086182968cdad0e8ca
Jake Rigby <jakewigby@gmail.com>
2014-09-18 16:19:00 +0100

    support no client callback

--- www/js/socketify.js
+++ www/js/socketify.js
@@ -5,16 +5,27 @@ function Socketify(ns, io) {
 			apis = {},
 			socket = io(ns);
 		socket.x = function() {
-			if (!apis[arguments[0]]) {
-				apis[arguments[0]] = true;
-				socket.on(arguments[0], function(err, result, cbid) {
-					if (typeof callbacks[cbid] === 'function') callbacks[cbid](err, result);
-				});
+			try {
+				if (typeof arguments[0] !== 'string') throw "api id not defined";
+				if (!apis[arguments[0]]) {
+					apis[arguments[0]] = true;
+					socket.on(arguments[0], function(err, result, cbid) {
+						if (typeof callbacks[cbid] === 'function') callbacks[cbid](err, result);
+					});
+				}
+				var cbid = Date.now()+Math.floor(Math.random()*1000000);
+				
+				if (typeof arguments[arguments.length-1] === 'function') {
+					callbacks[cbid] = arguments[arguments.length-1];
+					arguments[arguments.length-1] = cbid;					
+				} else {
+					arguments[arguments.length] = cbid;
+					arguments.length++;
+				}
+				socket.emit.apply(socket,arguments);
+			} catch (e) {
+				console.log(e)
 			}
-			var cbid = Date.now()+Math.floor(Math.random()*1000000);
-			callbacks[cbid] = arguments[arguments.length-1];
-			arguments[arguments.length-1] = cbid;
-			socket.emit.apply(socket,arguments);
 		}
 		return {socket:socket};
 	} else return new Socketify(ns, io);
